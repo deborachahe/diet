45eb651d7cb7b290627ca1ae9008aa1dc0f8aecc
Debora Chahe <deborachahe3@gmail.com>
2024-11-13 16:24:51 +0300

    anotherone

--- dietapp/urls.py
+++ dietapp/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings  # Import settings
+from django.conf.urls.static import static  # Import static
 from tracker import views  # Import views from the tracker app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),  # Add this line for the root URL
     path('tracker/', include('tracker.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
